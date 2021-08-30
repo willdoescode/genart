354468c5844b460cc7281c074513949ec77acba5
Will Lane <williamlane923@gmail.com>
2021-08-29 18:12:16 -0700

    base files and circles

---
+++ gen.py
@@ -0,0 +1,66 @@
+from PIL import Image, ImageDraw
+import random
+from typing import List, Tuple
+
+
+def random_hex() -> str:
+    def h(): return random.randint(0, 255)
+    return '#%02X%02X%02X' % (h(), h(), h())
+
+
+def random_hexs(amount: int) -> List[str]:
+    return [random_hex() for _ in range(amount)]
+
+
+def random_color_range() -> List[str]:
+    return random_hexs(random.randint(1, 20))
+
+
+class Shape:
+    x: int
+    y: int
+    color: str
+    line_width: int
+
+    def __init__(self, x: int, y: int, color: str, line_width: int = 1) -> None:
+        (self.x, self.y, self.color, self.line_width) = (x, y, color, line_width)
+
+    def draw(self, image_draw: ImageDraw, xy=Tuple[Tuple[int, int], Tuple[int, int]]) -> None:
+        image_draw.ellipse(
+            xy=xy,
+            outline=self.color,
+            width=self.line_width
+        )
+
+
+class Circle(Shape):
+    radius: float
+
+    def __init__(self, x: int, y: int, color: str, radius: float, line_width: int = 1) -> None:
+        super().__init__(x, y, color, line_width=line_width)
+        self.radius = radius
+
+    def draw(self, image_draw: ImageDraw) -> None:
+        return super().draw(image_draw=image_draw, xy=(
+            (self.x - self.radius, self.y - self.radius),
+            (self.x + self.radius, self.y + self.radius)
+        ))
+
+
+def main() -> None:
+    image = Image.new('RGBA', (1200, 1200), random_hex())
+    image_draw = ImageDraw.Draw(im=image)
+    circle = Circle(
+        x=1200/2,
+        y=1200/2,
+        radius=random.randint(0, 1200),
+        color=random_hex(),
+        line_width=10,
+    )
+    circle.draw(image_draw=image_draw)
+    image.save(f'img_{random.randint(0, 100000000000000000)}.png')
+    image.show()
+
+
+if __name__ == '__main__':
+    main()
