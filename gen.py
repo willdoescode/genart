7809467b5c3fedd86fe7fc6b6b51034f1f97877d
Will Lane <williamlane923@gmail.com>
2021-08-31 14:08:05 -0700

    square generation

--- gen.py
+++ gen.py
@@ -19,6 +19,7 @@ def random_color_range() -> List[str]:
 
 class DrawType(Enum):
     ELLIPSE = 0
+    RECTANGLE = 1
 
 
 class Shape:
@@ -38,6 +39,13 @@ class Shape:
                 width=self.line_width
             )
 
+        if draw_type == DrawType.RECTANGLE:
+            image_draw.rectangle(
+                xy=xy,
+                outline=self.color,
+                width=self.line_width
+            )
+
 
 class Circle(Shape):
     radius: float
@@ -52,18 +60,53 @@ class Circle(Shape):
             (self.x + self.radius, self.y + self.radius)
         ), draw_type=DrawType.ELLIPSE)
 
+    def __str__(self) -> str:
+        return super().__str__() + f': Radius: {self.radius}, Pos: ({self.x}, {self.y})'
+
+
+class Square(Shape):
+    width: int
+    height: int
+
+    def __init__(self, x: int, y: int, color: str, width: int, height: int, line_width: int = 1) -> None:
+        super().__init__(x, y, color, line_width=line_width)
+        (self.width, self.height) = (width, height)
+
+    def draw(self, image_draw: ImageDraw) -> None:
+        return super().draw(image_draw, xy=(
+            (self.x - self.width, self.y - self.height),
+            (self.x + self.width, self.y + self.height)
+        ), draw_type=DrawType.RECTANGLE)
+
+    def __str__(self) -> str:
+        return super().__str__() + f': Width: {self.width}, Height: {self.height}, Pos: ({self.x}, {self.y})'
+
 
 def main() -> None:
     image = Image.new('RGBA', (1200, 1200), random_hex())
     image_draw = ImageDraw.Draw(im=image)
-    circle = Circle(
-        x=1200/2,
-        y=1200/2,
-        radius=random.randint(0, 1200),
-        color=random_hex(),
-        line_width=10,
-    )
-    circle.draw(image_draw=image_draw)
+    for hexes in random_hexs(5):
+        square = Square(
+            x=1200/2,
+            y=1200/2,
+            width=random.randint(0, 1200),
+            height=random.randint(0, 1200),
+            color=hexes,
+            line_width=10,
+        )
+        square.draw(image_draw=image_draw)
+        print(square)
+
+        circle = Circle(
+            x=1200/2,
+            y=1200/2,
+            radius=random.randint(0, 1200),
+            color=hexes,
+            line_width=10,
+        )
+        circle.draw(image_draw=image_draw)
+        print(circle)
+
     image.save(f'img_{random.randint(0, 100000000000000000)}.png')
     image.show()
 
